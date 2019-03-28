ad34182346b616e4cf3614b2a360a75802b54ae9
Junsuk Oh <a811219@gmail.com>
2019-03-28 12:04:34 +0900

    코멘트 작성 인터페이스 작성

--- app/src/main/java/kr/co/woobi/imyeon/fragmentexam/Service.java
+++ app/src/main/java/kr/co/woobi/imyeon/fragmentexam/Service.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface Service {
     @GET("movie/readMovieList")
@@ -13,4 +13,11 @@ public interface Service {
 
     @GET("movie/readCommentList")
     Call <List<ReadCommentList>> listComment();
+
+    @GET("createComment")
+    Call <Object> createComment(@Query("id") int id,
+                                @Query("writer") String writer,
+                                @Query("time") String time,
+                                @Query("rating") double rating,
+                                @Query("contents") String contents);
 }
